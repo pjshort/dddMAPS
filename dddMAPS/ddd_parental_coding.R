a0b138c0d9e05ef02a82faa7bd50f1ea483db000
Patrick Short <pjshort42@gmail.com>
2016-03-01 14:29:59 +0000

    test script updated to fix typos - passes test run

--- dddMAPS/ddd_parental_coding.R
+++ dddMAPS/ddd_parental_coding.R
@@ -8,13 +8,13 @@ parental_gencode_snps = read.table("../data/gencode_parental_snps_+context.txt",
 #parental_gencode = read.table("../data/gencode_parental_alleles_FULL.txt", header = TRUE, sep = "\t")
 #parental_gencode = subset(parental_gencode, nchar(as.character(ref)) == 1 & nchar(as.character(alt)) == 1)
 
-synonymous_parental_vars = subset(parental_gencode, vep_consequence == "synonymous_variant")
+synonymous_parental_vars = subset(parental_gencode_snps, vep_consequence == "synonymous_variant")
 
 # fit linear model to synonymous variants
 maps_lm = maps_fit(synonymous_parental_vars)
 
 # get mutability adjusted proportion of singletons for all coding variants
-out = maps_adjust(variants = parental_gencode, split_factor = parental_gencode$vep_consequence, maps_lm = maps_lm, noncoding = FALSE)
+out = maps_adjust(variants = parental_gencode_snps, split_factor = parental_gencode_snps$vep_consequence, maps_lm = maps_lm, noncoding = FALSE)
 
 # plot MAPS for each vep consequence
 maps_ggplot(names(out$ps_adjusted), out$ps_adjusted, out$standard_error, already_ordered = FALSE)
